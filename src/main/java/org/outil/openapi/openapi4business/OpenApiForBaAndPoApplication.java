8e416a02ae3ef9bc8347efad31fcb29cd656491f
Jayesh Prajapati <jayeshecs@gmail.com>
2022-10-16 23:51:25 +0530

    Added openapi4business project

---
+++ src/main/java/org/outil/openapi/openapi4business/OpenApiForBaAndPoApplication.java
@@ -0,0 +1,13 @@
+package org.outil.openapi.openapi4business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiForBaAndPoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiForBaAndPoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/outil/openapi/openapi4business/OpenApiForBaAndPoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.outil.openapi.openapi4business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiForBaAndPoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
